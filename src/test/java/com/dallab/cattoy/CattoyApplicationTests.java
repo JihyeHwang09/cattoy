66c392b57ba6df2fc519dcf516a4fa9d1639ba5c
황지혜 <jh.hwang@crscube.co.kr>
2019-11-13 07:44:46 +0900

    first commit

---
+++ src/test/java/com/dallab/cattoy/CattoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dallab.cattoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CattoyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
